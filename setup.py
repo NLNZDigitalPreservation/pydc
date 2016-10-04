1e499d2aff6166866f1c711fac7653be1732a71a
Sean Mosely <sean.mosely@gmail.com>
2016-10-05 09:39:32 +1300

    removed "nose" from "install_requires" in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'Where to download it',
     'author_email': 'sean.mosely@gmail.com',
     'version': '0.1',
-    'install_requires': ['nose', 'lxml'],
+    'install_requires': ['lxml',],
     'packages': ['pydc'],
     'scripts': [],
     'name': 'pydc'
